0e1fc67d1d66508f7ef3d516ae73d99f270e43e4
deepakpathania <deepakpathania789@gmail.com>
2017-02-11 02:41:03 +0530

    Extension upper structure finalized

--- HealthBaitExtension/build/js/custom.js
+++ HealthBaitExtension/build/js/custom.js
@@ -2004,7 +2004,7 @@ if (typeof NProgress != 'undefined') {
 				  data: {
 					labels: ["January", "February", "March", "April", "May", "June", "July"],
 					datasets: [{
-					  label: "My First dataset",
+					  label: "Goals Completed in %",
 					  backgroundColor: "rgba(38, 185, 154, 0.31)",
 					  borderColor: "rgba(38, 185, 154, 0.7)",
 					  pointBorderColor: "rgba(38, 185, 154, 0.7)",
@@ -2012,17 +2012,7 @@ if (typeof NProgress != 'undefined') {
 					  pointHoverBackgroundColor: "#fff",
 					  pointHoverBorderColor: "rgba(220,220,220,1)",
 					  pointBorderWidth: 1,
-					  data: [31, 74, 6, 39, 20, 85, 7]
-					}, {
-					  label: "My Second dataset",
-					  backgroundColor: "rgba(3, 88, 106, 0.3)",
-					  borderColor: "rgba(3, 88, 106, 0.70)",
-					  pointBorderColor: "rgba(3, 88, 106, 0.70)",
-					  pointBackgroundColor: "rgba(3, 88, 106, 0.70)",
-					  pointHoverBackgroundColor: "#fff",
-					  pointHoverBorderColor: "rgba(151,187,205,1)",
-					  pointBorderWidth: 1,
-					  data: [82, 23, 66, 9, 99, 4, 2]
+					  data: [31, 74, 80, 39, 52, 85, 72]
 					}]
 				  },
 				});
